516fdba082e6ae8aeb0edacb5ce94c11203bb380
Partha-deuri <parthapratimdeuri4@gmail.com>
2025-03-19 13:01:41 +0530

    api_alive

--- shristi_backend/server.js
+++ shristi_backend/server.js
@@ -12,6 +12,10 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());  
 app.use(express.json()); 
 
+app.get('/api', (req, res) => {
+    res.status(200).json("i am alive");
+})
+
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/events', require('./routes/eventRoutes'));
 app.use('/api/dashboard', require('./routes/dashboardRoutes'));
